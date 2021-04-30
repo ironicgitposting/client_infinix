073adc5075c89b3eb107a117199d367e5497448e
Corentin <corentin.jules2018@campus-eni.fr>
2021-04-30 09:26:16 +0200

    Suppression du date pipe inutile

--- src/app/common/pipes/date.pipe.ts
+++
@@ -1,9 +0,0 @@
-// import { Pipe, PipeTransform } from '@angular/core';
-// import * as moment from 'moment';
-//
-// @Pipe({ name: 'dateFormat' })
-// export class DatePipe implements PipeTransform {
-//   transform(value: Date | moment.Moment, dateFormat: string): any {
-//     return moment(value).format(dateFormat);
-//   }
-// }
