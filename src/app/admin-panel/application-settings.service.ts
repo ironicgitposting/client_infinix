020d0e0eaced74e61ce04d17a27a1b7a1f4357f9
Patrick <prichard@herculepro.com>
2021-06-21 14:00:07 +0200

    wip settings

--- src/app/admin-panel/application-settings.service.ts
+++ src/app/admin-panel/application-settings.service.ts
@@ -1,10 +1,15 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { User } from '../users-list/user.model';
 
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ApplicationSettingsService {
 
-  private static getUserOptions(): null {
+  private static getSettings(): null {
     return null;
+  }
+
+  private static updateSetting(user: User, settingId: Number, flag: Boolean) {
+
   }
 }
