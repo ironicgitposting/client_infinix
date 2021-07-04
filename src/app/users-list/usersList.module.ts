31f0bbb57c1ef42da7321b63ebac25bd332fecec
romain <romain98.bedjai@gmail.com>
2021-07-05 01:07:53 +0200

    page user + ajustement autre page desktop

--- src/app/users-list/usersList.module.ts
+++ src/app/users-list/usersList.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatCardModule } from '@angular/material/card';
 import { UsersListComponent, DialogUser } from './usersList.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select'
+import { MatSelectModule } from '@angular/material/select'
 
 
 @NgModule({
@@ -19,6 +19,7 @@ import {MatSelectModule} from '@angular/material/select'
   ],
   imports: [
     BrowserModule,
+    MatCardModule,
     FormsModule,
     ReactiveFormsModule,
     MatExpansionModule,
